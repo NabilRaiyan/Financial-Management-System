2003d00b825afca3b5e6a633c769fb6cdea3ddc1
NabilRaiyan <rraiyan77@gmail.com>
2024-05-05 18:08:53 +0600

    adding home page middle section

--- layouts/views/home_view.php
+++ layouts/views/home_view.php
@@ -6,7 +6,7 @@
 <div class="upper-section">
     <?php include "./header.php"; ?>
 
-    <div class="left-upper-section" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+    <div class="left-upper-section" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
         
         <h1 class="upper-title">
             Best Way <span class="title-word-color-black">To</span> <br>
@@ -23,13 +23,13 @@
 
 <!-- middle section  -->
 
-<div class="middle-section">
+<div class="middle-section" data-aos="fade-up" data-aos-duration="1000">
     <h1 class="middle-title">Why Choose Us?</h1>
     <p class="middle-sub-title">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel alias doloribus, modi enim assumenda ex facere expedita?
     </p>
-    <div class="middle-sub-section">
-    <img class="middle-img-1" src="../assets/middle-img-1.png" alt="" srcset="">
+    <div class="middle-sub-section" data-aos="fade-right" data-aos-duration="1000">
+    <img class="middle-img-1" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" src="../assets/middle-img-1.png" alt="" srcset="">
         <p class="middle-description-1">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, ut quibusdam. 
             Nesciunt vitae nulla officia blanditiis, dignissimos enim laudantium assumenda minima, quisquam architecto corporis, necessitatibus quaerat iusto facere repellat recusandae.
@@ -37,7 +37,7 @@
     </div>
 
 
-    <div class="middle-sub-section">
+    <div class="middle-sub-section" data-aos="fade-down" data-aos-duration="1000">
     <img class="middle-img-2" src="../assets/middle-img-2.png" alt="" srcset="">
         <p class="middle-description-2">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, ut quibusdam. 
@@ -46,8 +46,8 @@
     </div>
 
 
-    <div class="middle-sub-section">
-    <img class="middle-img-3" src="../assets/middle-img-3.png" alt="" srcset="">
+    <div class="middle-sub-section" data-aos="fade-left" data-aos-duration="1000">
+    <img class="middle-img-3" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000" src="../assets/middle-img-3.png" alt="" srcset="">
         <p class="middle-description-3">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, ut quibusdam. 
             Nesciunt vitae nulla officia blanditiis, dignissimos enim laudantium assumenda minima, quisquam architecto corporis, necessitatibus quaerat iusto facere repellat recusandae.
