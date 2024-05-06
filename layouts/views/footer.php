68500caa25210b8a413b44ad2d9d85a53674b115
NabilRaiyan <rraiyan77@gmail.com>
2024-05-06 22:15:57 +0600

    Adding services page card

--- layouts/views/footer.php
+++ layouts/views/footer.php
@@ -11,15 +11,20 @@
             <li class="nav-menu"><a href="#" class="menu-link">Services</a></li><br>
             <li class="nav-menu"><a href="#" class="menu-link">About Us</a></li><br>
             <li class="nav-menu"><a href="#" class="menu-link">Contact us</a></li><br>
-        </div>
 
+        </div>
         <div class="section-3">
             <p class="section-3-description">
                 Want a best financial tips? <br>
                 We send our tips on every week in a newsletter. <br>
+            </p>
+            <form action="" method="">
                 <input type="text" placeholder="Your-Email" class="newsletter-input">
                 <button type="submit" class="newsletter-submit-btn">Sign Up</button>
-            </p>
+            </form>
+            
         </div>
+        
+
     </div>
 </footer>
