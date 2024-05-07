01ba93f144d098d515e0cee58aa743a4131a4448
NabilRaiyan <rraiyan77@gmail.com>
2024-05-07 22:32:35 +0600

    Adding contact us page

--- layouts/views/contact_view.php
+++ layouts/views/contact_view.php
@@ -2,9 +2,26 @@
 <html lang="en">
 <title>Contact Us</title>
 
+<?php include "./header.php" ?>
 
+<!-- contact us page -->
 
+<h1 class="contact-title">
+    Get In Touch
+</h1>
+<p class="contact-subtitle">Let's make something big together!</p>
+<div class="contact-body">
+    <div class="left-section">
+        <h1 class="left-section-title">Mail Us</h1>
+        <p class="left-section-body">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet molestias quisquam dolor maxime? Necessitatibus nulla, 
+            deleniti quaerat dolorum laboriosam vero labore, iste adipisci amet placeat odit doloremque natus beatae inventore.
+        </p>
+        <p class="phone-number"><i class="fa-solid fa-2x fa-phone-volume"></i>+1-780-2843810</p>
+        <p class="address"><i class="fa-solid fa-2x fa-location-dot"></i>123 Main Street <br> Anytown, USA</p>
 
+    </div>
+</div>
 
 <!-- animation on scroll js  -->
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
