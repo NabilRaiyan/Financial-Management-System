5915dadd8147ec1ad8244388d99074e59d9d471f
NabilRaiyan <rraiyan77@gmail.com>
2024-05-08 17:03:08 +0600

    Adding small business owner registration

--- layouts/views/service_card_view.php
+++ layouts/views/service_card_view.php
@@ -5,7 +5,7 @@
         <h1 class="card-title">Single User</h1>
         <h2 class="card-subtitle">Free</h2>
         <p class="money">$10 <br> per user/month</p>
-        <a class="sign-up-btn" href="#">Sign Up</a>
+        <a class="sign-up-btn" href="../../small_business_owner/views/registration_small_business_view.php">Sign Up</a>
 
         <div class="feature-list">
             <li><i class="fa-regular fa-circle-check"></i>100% free</li>
---
+++ small_business_owner/views/registration_small_business_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<title>Forgot Password?</title>
+<link rel="stylesheet" href="../../layouts/css/styles.css">
+<?php include "../../layouts/views/header.php" ?>
+
+
+
+
+
+
+
+<!-- animation on scroll js  -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+    <?php include "../../layouts/views/footer.php"; ?>
+</body>
+</html>
